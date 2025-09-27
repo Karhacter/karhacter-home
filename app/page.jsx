c7403b35e5a98dda2e4c184375e7cc3a2359ac52
Karhacter <109360401+Karhacter@users.noreply.github.com>
2025-09-28 01:59:07 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Frontend Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Karhacter</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">Lorem Puntimasd</p>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ app/resume/page.jsx
@@ -0,0 +1,5 @@
+const Resume = () => {
+  return <div>Resume pages</div>;
+};
+
+export default Resume;
